f97efc4c9781021d5791045df6dd5af0a231a33e
DragnelDev <scandragnel@gmail.com>
2025-07-30 14:01:17 -0400

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,10 +1,10 @@
 <?php
 // api/config.php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); // Cambia esto por tu usuario de BD
-define('DB_PASSWORD', '');     // Cambia esto por tu contraseña de BD
-define('DB_NAME', 'mi_agenda_db'); // Cambia esto por el nombre de tu BD
+define('DB_SERVER', 'nozomi.proxy.rlwy.net');
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', 'UvCnpVFeTNeJEGgDDsBUPizBKBPWlxeO');     // contraseña de BD
+define('DB_NAME', 'railway'); //nombre de tu BD
 
 // Intentar conexión a la base de datos MySQL
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
